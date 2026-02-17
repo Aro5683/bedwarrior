cf0f2bc9305d57675a1644d50592f7b94e80d7ad
Aro5683 <arotesting54@gmail.com>
2026-02-16 22:23:04 -0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,18 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", e=>{
   e.waitUntil(
-    caches.open("bed-cache").then(cache => {
+    caches.open("bed-cache").then(cache=>{
       return cache.addAll([
         "index.html",
         "app.js",
-        "codes.json",
-        "master.bedx"
+        "master.bedx",
+        "codes.json"
       ]);
     })
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(r=>r||fetch(e.request))
   );
 });
